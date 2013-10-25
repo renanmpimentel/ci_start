260fba92b3dc3dddc3a57c8bfb9c0db344bcb057
Ronei Chiarandi <ronei.chiarandi@gmail.com>
2013-10-25 14:43:32 -0200

    CI_Start com Twitter Bootstrap 3.0 CDN

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,3 +1,21 @@
 	<body>
 	</body>
+	<!-- Scripts -->
+
+    <!-- Google jQuery API -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <!-- Bootstrap Script // Latest compiled and minified JavaScript -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-wip/js/bootstrap.min.js"></script>
+    <!-- Google Analytics -->
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-XXXXXX-Y']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
 </html>
