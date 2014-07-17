066501e08af4ba0cb100c5397acebf5729f06ca2
Caio Mariano <substancex@gmail.com>
2014-07-16 23:31:22 -0300

    voltando o autoload da database

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('session', 'form_validation');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'form');
+$autoload['helper'] = array('database','url', 'form');
 
 
 /*
